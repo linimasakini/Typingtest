48957f26044359f0ccfdbe59ed6fe4bc7a93b9d3
Linimasa <116949567+linimasakini@users.noreply.github.com>
2023-07-20 16:05:48 +0700

    Add files via upload

---
+++ LV 1/script/paragraph.js
@@ -0,0 +1,9 @@
+const paragraphs = [
+    "aku adalah anak sehat",
+    "namun aku juga dapat sakit",
+    "disaat hari minggu aku,",
+    "menghisap sabu di jalanan",
+    "lorem ipsum",
+    "dolor amet",
+    "sin cos tan"
+];
