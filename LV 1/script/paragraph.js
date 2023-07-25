b5fe9d20829728001ece6f74672f3f64d6a12959
Linimasa <116949567+linimasakini@users.noreply.github.com>
2023-07-25 16:45:57 +0700

    Add files via upload

--- LV 1/script/paragraph.js
+++ LV 1/script/paragraph.js
@@ -1,8 +1,1 @@
-const paragraphs = [
-    "aku adalah anak sehat",
-    "Push yourself, because no one else is going to do it for you.",
-    "Failure is the condiment that gives success its flavor.",
-    "Wake up with determination. Go to bed with satisfaction.",
-    "It's going to be hard, but hard does not mean impossible.",
-    "Learning never exhausts the mind."
-];
+const paragraphs = ["aku adalah anak sehat"];
--- LV 1/script/script.js
+++ LV 1/script/script.js
@@ -1,174 +1,160 @@
-// define waktu pengerjaan
+// Limit Waktu
 let TIME_LIMIT = 10;
 
-// define yang dibutuhkan dalam html
-let timer_text = document.querySelector(".timer-number");
-let wpm_text = document.querySelector(".wpm-number");
-let cpm_text = document.querySelector(".cpm-number");
-let error_text = document.querySelector(".error-number");
-let accurate_text = document.querySelector(".accurate-number");
+// element yang dibutuhkan
+let timer_text = document.querySelector(".curr_time");
+let accuracy_text = document.querySelector(".curr_accuracy");
+let error_text = document.querySelector(".curr_errors");
+let cpm_text = document.querySelector(".curr_cpm");
+let wpm_text = document.querySelector(".curr_wpm");
+let quote_text = document.querySelector(".quote");
+let input_area = document.querySelector(".input_area");
+let restart_btn = document.querySelector(".restart_button");
 let cpm_group = document.querySelector(".cpm");
 let wpm_group = document.querySelector(".wpm");
 let error_group = document.querySelector(".errors");
-let accurate_group = document.querySelector(".accurate");
-let input_area = document.querySelector(".input_area");
-let quote_text = document.querySelector(".typing-text")
-let restart_btn = document.querySelector(".reset_button");
+let accuracy_group = document.querySelector(".accurate");
 
 let timeLeft = TIME_LIMIT;
 let timeElapsed = 0;
 let total_errors = 0;
-let totalErrors = 0;
 let errors = 0;
 let accuracy = 0;
 let characterTyped = 0;
 let current_quote = "";
-let quote_not = 0;
+let quoteNo = 0;
 let timer = null;
 
 function updateQuote() {
-    quote_text.textContent = null;
-    current_quote = paragraphs[quote_not];
+  quote_text.textContent = null;
+  current_quote = paragraphs[quoteNo];
 
-    // separate
-    current_quote.split('').forEach(char => {
-        const charSpan = document.createElement('span')
-        charSpan.innerText = char
-        quote_text.appendChild(charSpan)
-    })
+  //   separate karakter dan buat element
+  current_quote.split("").forEach((char) => {
+    const charSpan = document.createElement("span");
+    charSpan.innerText = char;
+    quote_text.appendChild(charSpan);
+  });
 
-    // roll ke quote pertama
-    if (quote_not < paragraphs.length - 1)
-    quote_not++;
-    else
-    quote_not = 0;
+  //   roll ke quote pertama
+  if (quoteNo < paragraphs.length - 1) quoteNo++;
+  else quoteNo = 0;
 }
 
 function processCurrentText() {
+  // dapatkan inputan text dan menjadikan split
+  curr_input = input_area.value;
+  curr_input_array = curr_input.split("");
 
-    // input user
-    curr_Input = input_area.value;
-    curr_Input_Array = curr_Input.split('');
+  characterTyped++;
 
-    // increment total karakter
-    characterTyped++;
+  errors = 0;
 
-    errors = 0
+  quoteSpanArray = quote_text.querySelectorAll("span");
+  quoteSpanArray.forEach((char, index) => {
+    let typedChar = curr_input_array[index];
 
-    quoteSpanArray = quote_text.querySelectorAll('span');
-    quoteSpanArray.forEach((char, index) => {
-        let typedChar = curr_Input_Array[index]
-
-        // karakter tidak diketik
-        if( typedChar == null ) {
-            char.classlist.remove('correct_char');
-            char.classlist.remove('incorrect_char');
-
-            // karakter benar
-        } else if (typedChar === char.innerText) {
-            char.classList.add('correct_char');
-            char.classList.add('incorrect_char');
-
-            // karakter salah
-        } else {
-            char.classList.add('correct_char');
-            char.classList.remove('incorrect_char');
-        } 
-
-        // increment karakter erors
-        errors++;
-    });
-
-    // menampilkan banyakya eror
-    error_text.textContent = totalErrors + errors;
-
-    // update akurasi text
-    let correctCharacters = (characterTyped - (totalErrors + errors));
-    let accuracyVal = ((correctCharacters / characterTyped) * 100);
-    accurate_text.textContent = Math.round(accuracyVal);
-
-    // jika teks telah terketik maka tidak melihatkan eror
-    if(curr_Input.length == current_quote.length) {
-        updateQuote();
-
-        // update total eror
-        totalErrors += errors;
-
-        // menghapus input area
-        input_area.value = "";
-    }
-};
-
-function updateTimer() {
-    if (timeLeft > 0) {
-
-        // mengurangi waktu
-        timeLeft--;
-
-        // menambah waktu elapsed
-        timeElapsed++;
-
-        // update waktu
-        timer_text.textContent = timeLeft + "s";
+    // karakter tidak diketik
+    if (typedChar == null) {
+      char.classList.remove("correct_char");
+      char.classList.remove("incorrect_char");
+      //   karakter benar
+    } else if (typedChar === char.innerText) {
+      char.classList.add("correct_char");
+      char.classList.remove("incorrect_char");
+      // karakter salah
     } else {
-        // finish game
-        finishGame();
+      char.classList.add("incorrect_char");
+      char.classList.remove("correct_char");
+
+      errors++;
     }
-}
+  });
 
+  //   display number error
+  error_text.textContent = total_errors + errors;
 
+  // update accuracy
+  let correctCharacters = characterTyped - (total_errors + errors);
+  let accuracyVal = (correctCharacters / characterTyped) * 100;
+  accuracy_text.textContent = Math.round(accuracyVal);
 
-function startGame(){
-    resetValues();
+  //   jika belum semua maka respektif eror
+  if (curr_input.length == current_quote.length) {
     updateQuote();
 
-    // menghapus yang lama dan membuat yang baru
-    clearInterval(timer);
-    timer = setInterval(updateTimer, 1000);
+    // total error
+    total_errors += errors;
+
+    // clear input area
+
+    input_area.value = "";
+  }
+}
+
+function updateTimer() {
+  if (timeLeft > 0) {
+    timeLeft--;
+
+    timeElapsed++;
+
+    timer_text.textContent = timeLeft + "s";
+  } else {
+    finishGame();
+  }
 }
 
 function finishGame() {
-    // menghentikan waktu dari loop
-    clearInterval(timer);
+  // stop timer
+  clearInterval(timer);
 
-    // disable input area
-    input_area.disabled = true;
+  //   disale input area
+  input_area.disabled = true;
 
-    // text akhir
-    quote_text.textContent = "click restart untuk bermain lagi";
+  // menampilkan text
+  quote_text.textContent = "klik pada area yang ditentukan";
 
-    // memunculkan display restart
-    restart_btn.style.display = "block";
+  //   display restart button
+  restart_btn.style.display = "block";
 
-    // kalkulasi cpm dan wpm
-    cpm = Math.round(((characterTyped / timeElapsed) * 60));
-    wpm = Math.round((((characterTyped / 5) / timeElapsed) * 60));
+  //   cpm dan wpm kalkulate
+  cpm = Math.round((characterTyped / timeElapsed) * 60);
+  wpm = Math.round((characterTyped / 5 / timeElapsed) * 60);
 
-    // update cpm dan wpm text
-    cpm_text.textContent = cpm;
-    wpm_text.textContent = wpm;
-    
-    // display cpm wpm
-    cpm_group.style.display = "block";
-    wpm_group.style.display = "block";
+  //   update cpm dan wpm
+  cpm_text.textContent = cpm;
+  wpm_text.textContent = wpm;
+
+  //   display cpm dan wpm
+  cpm_group.style.display = "block";
+  wpm_group.style.display = "block";
+}
+
+function startGame() {
+  resetValues();
+  updateQuote();
+
+  // hilangkan dan mulai waktu
+  clearInterval(timer);
+  timer = setInterval(updateTimer, 1000);
 }
 
 function resetValues() {
-    timeLeft = TIME_LIMIT;
-    timeElapsed = 0;
-    errors = 0;
-    totalErrors = 0;
-    accuracy = 0;
-    characterTyped = 0;
-    quote_not = 0;
-    input_area.disabled = false;
+  timeLeft = TIME_LIMIT;
+  timeElapsed = 0;
+  errors = 0;
+  total_errors = 0;
+  accuracy = 0;
+  characterTyped = 0;
+  quoteNo = 0;
+  input_area.disabled = false;
 
-    input_area.value = "";
-    quote_text.textContent = "Klik pada area ditentukan untuk memulai...";
-    accuracy.textContent = 100;
-    timer_text.textContent = timeLeft + 's';
-    error_text.textContent = 0;
-    restart_btn.style.display = "none";
-    cpm_group.style.display = "none";
-    wpm_group.style.display = "none";
+  input_area.value = "";
+  quote_text.textContent = "Klik pada area yang ditentukan";
+  accuracy_text.textContent = 100;
+  timer_text.textContent = timeLeft + "s";
+  error_text.textContent = 0;
+  restart_btn.style.display = "none";
+  cpm_group.style.display = "none";
+  wpm_group.style.display = "none";
 }
