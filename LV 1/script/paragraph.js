c482614fca7ac55e5877461ef6a95b4b8b43941f
Linimasa <116949567+linimasakini@users.noreply.github.com>
2023-07-26 07:47:37 +0700

    Update paragraph.js

--- LV 1/script/paragraph.js
+++ LV 1/script/paragraph.js
@@ -1,1 +1,3 @@
-const paragraphs = ["aku adalah anak sehat"];
+const paragraphs = ["aku adalah anak sehat",
+                   "Asep kependekan dari kasep yang berarti cakep",
+                   "Baby shark dudududududu baby shark dudududududu"];
